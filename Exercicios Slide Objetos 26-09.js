8b7c36151726f97dec11a99edd40dcee52110370
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-09-26 11:52:17 -0300

    Add files via upload

---
+++ Exercicios Slide Objetos 26-09.js
@@ -0,0 +1,97 @@
+//Exercicio Slide 1 
+
+/* const filme = {
+ nome: `Todo Mundo em Panico` , 
+ direção: 'Keenen Ivory Wayans' , 
+ dataLançamento:`22 de setembro de 2000` ,  
+ elenco:['Anna Faris, Marlon Wayans, Regina Hall, Shawn Wayans, Keenen Ivory Wayans, Shannon Elizabeth'], 
+ visto: true
+}
+
+//notação ponto
+console.log(filme.nome) 
+console.log(filme.dataLançamento)
+
+//notação colchetes
+console.log(filme["elenco"])
+console.log(filme["visto"])
+console.log(filme["direção"])
+
+*/
+
+
+//Exercicio Slide 2
+/* const pessoa = {
+    nome: "Gustavo" , 
+    idade: 17 , 
+    generoMusical: "Rap"
+   }
+
+   console.log ("O nome da pessoa é " + (pessoa.nome) + " ele tem " + (pessoa.idade) + " anos e gosta muito de " + (pessoa.generoMusical) */ 
+
+
+
+//Exercicio Slide 3 
+/* const filme = {
+    nome: `Todo Mundo em Panico` , 
+    direção: 'Keenen Ivory Wayans' , 
+    dataLançamento:`22 de setembro de 2000` ,  
+    elenco:["Anna Faris", "Marlon Wayans", "Regina Hall", "Shawn Wayans", "Keenen Ivory Wayans", "Shannon Elizabeth"] ,
+    visto: true
+   }
+   
+   filme.personagem = ["Cindy Campbell", "Shorty Meeks", "Brenda Meeks", "Ray Wilkins", "Doofy Gilmore", "Buffy Gilmore"]
+
+   //notação ponto
+   console.log(filme.nome) 
+   console.log(filme.dataLançamento)
+   
+   //notação colchetes
+   console.log(filme["elenco"])
+   console.log(filme["visto"])
+   console.log(filme["direção"])
+
+   console.log(filme.elenco[0], "-", filme.personagem [0])
+   console.log(filme.elenco[1], "-", filme.personagem [1])
+   console.log(filme.elenco[2], "-", filme.personagem [2])
+   console.log(filme.elenco[3], "-", filme.personagem [3])
+   console.log(filme.elenco[4], "-", filme.personagem [4])
+   console.log(filme.elenco[5], "-", filme.personagem [5])
+
+   filme.elenco[0] = "Xuxa"
+   console.log(filme.elenco)
+
+   console.log (filme)
+
+
+   //Exercicio Slide */ 
+
+
+
+//Exercicio Slide 4
+
+ /* const pessoa = {
+    nome: "Gustavo" , 
+    idade: 17 , 
+    generoMusical: "Rap"
+   }
+
+   // console.log ("O nome da pessoa é " + (pessoa.nome) + " ele tem " + (pessoa.idade) + " anos e gosta muito de " + (pessoa.generoMusical)) 
+
+
+
+   function adldlk (pessoa) {
+    const pessoa1 = {
+    ...pessoa ,
+    comidas:["Pastel", "Pizza", "Pão"], 
+     amigo: {
+        nomeamg: "Deus" , 
+        idadeamg: 999999999 ,
+        
+    
+
+    }
+    } 
+    console.log (" O nome da pessoa é " + pessoa1.nome + " e suas comidas preferidas são " + pessoa1.comidas + " e seu melhor amigo se chama " + pessoa1.amigo.nomeamg + " e tem " + pessoa1.amigo.idadeamg + " anos " ) 
+   }
+   console.log(adldlk(pessoa)) */ 
